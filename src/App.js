ce5ee45d1d9a9b8decdf9435693e105b3872a15c
gilyeon00 <12venus08@naver.com>
2023-02-12 22:51:36 +0900

    feat: 컴포넌트 설계및 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signin from './component/Signin'
+import Signup from './component/Signup'
+import Todo from './component/Todo'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signin" element={<Signin/>} />
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/todo" element={<Todo/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
